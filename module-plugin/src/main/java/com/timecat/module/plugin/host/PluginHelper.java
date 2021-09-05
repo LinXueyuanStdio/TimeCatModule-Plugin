d95afa9e161be3d3042662a8d83292cd08550938
linxueyuanstdio <761516186@qq.com>
2021-09-05 14:51:40 +0800

    save

--- module-plugin/src/main/java/com/timecat/module/plugin/host/PluginHelper.java
+++ module-plugin/src/main/java/com/timecat/module/plugin/host/PluginHelper.java
@@ -1,30 +1,14 @@
-/*
- * Tencent is pleased to support the open source community by making Tencent Shadow available.
- * Copyright (C) 2019 THL A29 Limited, a Tencent company.  All rights reserved.
- *
- * Licensed under the BSD 3-Clause License (the "License"); you may not use
- * this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- *     https://opensource.org/licenses/BSD-3-Clause
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.timecat.module.plugin.host;
 
 import android.content.Context;
 
-import org.apache.commons.io.FileUtils;
+import com.timecat.component.commonsdk.utils.utils.FileUtil;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -33,12 +17,12 @@ public class PluginHelper {
     /**
      * 动态加载的插件管理apk
      */
-    public final static String sPluginManagerName = "pluginmanager.apk";
+    public final static String sPluginManagerName = "plugin-manager.apk";
 
     /**
      * 动态加载的插件包，里面包含以下几个部分，插件apk，插件框架apk（loader apk和runtime apk）, apk信息配置关系json文件
      */
-    public final static String sPluginZip = BuildConfig.DEBUG ? "plugin-debug.zip" : "plugin-release.zip";
+    public final static String sPluginZip = "plugin-release.zip";
 
     public File pluginManagerFile;
 
@@ -75,15 +59,14 @@ public class PluginHelper {
     private void preparePlugin() {
         try {
             InputStream is = mContext.getAssets().open(sPluginManagerName);
-            FileUtils.copyInputStreamToFile(is, pluginManagerFile);
+            OutputStream out = new FileOutputStream(pluginManagerFile);
+            FileUtil.copy(is, out);
 
             InputStream zip = mContext.getAssets().open(sPluginZip);
-            FileUtils.copyInputStreamToFile(zip, pluginZipFile);
-
+            OutputStream out2 = new FileOutputStream(pluginZipFile);
+            FileUtil.copy(zip, out2);
         } catch (IOException e) {
             throw new RuntimeException("从assets中复制apk出错", e);
         }
     }
-
-
 }
