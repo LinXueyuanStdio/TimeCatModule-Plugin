c13a50db8411aee2953eda2f030ccc73710f9ca3
linxueyuanstdio <761516186@qq.com>
2021-09-05 17:01:58 +0800

    Update PluginServiceImpl.java

--- module-plugin/src/main/java/com/timecat/module/plugin/core/PluginServiceImpl.java
+++ module-plugin/src/main/java/com/timecat/module/plugin/core/PluginServiceImpl.java
@@ -40,126 +40,12 @@ public class PluginServiceImpl implements PluginService {
     @Override
     public void start(InputToPlugin input, OutputFromPlugin output) {
 
-        String pluginFileName = input.filename();
-        String downloadUrl = input.downloadUrl();
-        boolean loadAfterDownload = input.isPlugin();
-        output.setEnabled(false);
-        if (existPlugin(pluginFileName)) {
-            // 已下载
-            if (loadAfterDownload) {
-                loadPlugin(pluginFileName, input, output);
-            } else {
-                // manager 不能被加载
-                output.setEnabled(true);
-            }
-        } else {
-            // 未下载
-            // manager 不能被加载
-            LogUtil.se(pluginFileName + " <- " + downloadUrl);
-            String downloadFile = PictureBedPluginConstants.getPluginAbsPath(context, pluginFileName);
-            downloadFile(downloadUrl, downloadFile, new DownloadListener() {
-                @Override
-                public void onStart() {
-                    output.onDownloadStart();
-                }
-
-                @Override
-                public void onProgress(int currentLength, int total) {
-                    output.onDownloadProgress(currentLength, total);
-                }
-
-                @Override
-                public void onFinish(String localPath) {
-                    LogUtil.se("");
-                    output.onDownloadSuccess();
-                    if (loadAfterDownload) {
-                        LogUtil.se("");
-                        loadPlugin(pluginFileName, input, output);
-                    } else {
-                        output.setEnabled(true);
-                    }
-                }
-
-                @Override
-                public void onFailure() {
-                    LogUtil.se("");
-                    output.onDownloadFail("下载失败！");
-                }
-            });
-        }
-    }
-
-    private Thread mThread;
-    public void downloadFile(String fromUrl, String toFilePath, final DownloadListener downloadListener) {
-        File mFile = new File(toFilePath);
-        if (!FileUtils.isFileExists(mFile) && FileUtils.createOrExistsFile(mFile)) {
-//            getPluginService().downloadFile(fromUrl)
-//                    .subscribeOn(Schedulers.newThread())//请求在新的线程中执行
-//                    .observeOn(AndroidSchedulers.mainThread())//最后在主线程中执行
-//                    .subscribe(new Observer<ResponseBody>() {
-//                        @Override
-//                        public void onComplete() {
-//                        }
-//
-//                        @Override
-//                        public void onError(Throwable e) {
-//                            if (e != null) e.printStackTrace();
-//                            downloadListener.onFailure(); //下载失败
-//                        }
-//
-//                        @Override
-//                        public void onSubscribe(Disposable d) {
-//                        }
-//
-//                        @Override
-//                        public void onNext(ResponseBody response) {
-//                            //下载文件放在子线程
-//                            mThread = new Thread(() -> DownloadUtil.writeFile2Disk(response, mFile, downloadListener));
-//                            mThread.start();
-//                        }
-//                    });
-        } else {
-            downloadListener.onFinish(toFilePath); //下载完成
-        }
     }
 
     @Override
     public boolean existPlugin(String filename) {
         String path = PictureBedPluginConstants.getPluginAbsPath(context, filename);
         return !TextUtils.isEmpty(path) && new File(path).exists();
-    }
-
-    private void loadPlugin(String pluginFileName, InputToPlugin input, OutputFromPlugin output) {
-//        PluginManager pluginManager = Shadow.getPluginManager();
-        //        if (pluginManager == null) {
-        //            output.onEnterFail("未找到插件管理器，请安装后重试");
-        //            return;
-        //        }
-        //        Uri uri = input.uri();
-        //        Bundle extra = input.extra();
-        //        String action = input.action();
-        //        String partKey = input.partKey();
-        //        String activityClassName = input.activityClassName();
-        //        String filepath = PictureBedPluginConstants.getPluginAbsPath(context, pluginFileName);
-        //        Bundle bundle = getBundle(filepath, partKey, activityClassName, extra, uri, action);
-        //
-        //        pluginManager.enter(context, PluginHub.FROM_ID_START_ACTIVITY, bundle, new EnterCallback() {
-//            @Override
-//            public void onShowLoadingView(View view) {
-//                //显示Manager传来的Loading页面
-//                output.onLoading(view);
-//            }
-//
-//            @Override
-//            public void onCloseLoadingView() {
-//                output.onCloseLoadingView();
-//            }
-//
-//            @Override
-//            public void onEnterComplete() {
-//                output.onEnterComplete();
-//            }
-//        });
     }
 
     private Bundle getBundle(String zipAbsPathForPlugin,
