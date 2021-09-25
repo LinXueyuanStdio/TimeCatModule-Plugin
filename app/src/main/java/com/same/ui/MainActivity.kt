21dd5f327552f63f960c2e7676dd4594e952bf0d
linxueyuanstdio <761516186@qq.com>
2021-09-25 13:13:51 +0800

    Update MainActivity.kt

--- app/src/main/java/com/same/ui/MainActivity.kt
+++ app/src/main/java/com/same/ui/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.same.ui
 
-import android.content.Intent
 import android.view.ViewGroup
 import com.timecat.component.router.app.NAV
 import com.timecat.identity.readonly.RouterHub
 import com.timecat.layout.ui.business.form.Next
 import com.timecat.middle.setting.BaseSettingActivity
-import com.timecat.module.plugin.PluginCloudActivity
 
 class MainActivity : BaseSettingActivity() {
     override fun title(): String = "Debug"
     override fun addSettingItems(container: ViewGroup) {
         container.Next("插件市场") {
-            startActivity(Intent(this, PluginCloudActivity::class.java))
+            NAV.go(RouterHub.PLUGIN_PluginCloudActivity)
         }
         container.Next("皮肤市场") {
             NAV.go(RouterHub.SKIN_SkinActivity)
