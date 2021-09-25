d80a5fbef1006b6d84b9e245c56db4a995d11504
linxueyuanstdio <761516186@qq.com>
2021-09-25 13:03:38 +0800

    Update MainActivity.kt

--- app/src/main/java/com/same/ui/MainActivity.kt
+++ app/src/main/java/com/same/ui/MainActivity.kt
@@ -1,66 +1,16 @@
 package com.same.ui
 
-import android.app.Activity
 import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
+import com.timecat.layout.ui.business.form.Next
+import com.timecat.middle.setting.BaseSettingActivity
 import com.timecat.module.plugin.PluginCloudActivity
-import com.xiaojinzi.component.impl.*
 
-class MainActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val linearLayout = LinearLayout(this)
-        linearLayout.orientation = LinearLayout.VERTICAL
-        linearLayout.addView(createButton("插件市场") {
+class MainActivity : BaseSettingActivity() {
+    override fun title(): String = "Debug"
+    override fun addSettingItems(container: ViewGroup) {
+        container.Next("插件市场") {
             startActivity(Intent(this, PluginCloudActivity::class.java))
-        })
-        setContentView(linearLayout)
-    }
-
-    private fun createButton(name: String, path: String): Button {
-        val button = createButton(name)
-        button.setOnClickListener { go(path) }
-        return button
-    }
-
-    private fun createButton(name: String, onClickListener: View.OnClickListener): Button {
-        val button = createButton(name)
-        button.setOnClickListener(onClickListener)
-        return button
-    }
-
-    private fun createButton(name: String): Button {
-        val button = Button(this)
-        button.text = name
-        val layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        button.layoutParams = layoutParams
-        button.gravity = Gravity.CENTER
-        return button
-    }
-
-    private fun go(path: String) {
-        Router.with().hostAndPath(path)
-            .forward(object : Callback {
-                override fun onSuccess(result: RouterResult) {}
-                override fun onEvent(
-                    successResult: RouterResult?,
-                    errorResult: RouterErrorResult?
-                ) {
-                }
-
-                override fun onCancel(originalRequest: RouterRequest?) {}
-                override fun onError(errorResult: RouterErrorResult) {
-                    Log.e("ui", errorResult.error.toString())
-                }
-            })
+        }
     }
 }
