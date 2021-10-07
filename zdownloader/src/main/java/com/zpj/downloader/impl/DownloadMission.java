4b07b94b72d2ca043e4b99961d682b366b96c9c7
linxueyuanstdio <761516186@qq.com>
2021-10-07 21:30:12 +0800

    save

---
+++ zdownloader/src/main/java/com/zpj/downloader/impl/DownloadMission.java
@@ -0,0 +1,15 @@
+package com.zpj.downloader.impl;
+
+import androidx.annotation.Keep;
+
+import com.zpj.downloader.BaseMission;
+
+import java.io.Serializable;
+
+/**
+ * @author Z-P-J
+ */
+@Keep
+public final class DownloadMission extends BaseMission<DownloadMission> implements Serializable {
+
+}
